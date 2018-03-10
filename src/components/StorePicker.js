c6f1541f8ef49f5b3714be3bb69ce2c428ade08b
moystre <moystre@gmail.com>
2018-03-10 17:54:56 +0100

    button & input for Store-Selector

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,15 @@ import { render } from 'react-dom';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Hellow</p>
+        // comment outside of jsx
+        return (
+        <form className="store-selector">
+            { /* Legit comment inside a jsx (return) */}
+            <h2>Please Enter a Store</h2>
+            <input type="text" required placeholder="Store Name"/>
+            <button type="submit">Visit Store</button>
+        </form>
+        )
     }
 }
 
